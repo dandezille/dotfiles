9f83f6865f902493add707931d00abeeed9e5592
Dan de Zille <dan@ddez.net>
2019-10-12 15:59:53 +0200

    Add rust binaries to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,3 @@
 set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/lib/ruby/gems/2.6.0/bin" $fish_user_paths
+set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths

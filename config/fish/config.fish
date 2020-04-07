e4a7b081e1cae9442c3322ae76480bc603d6d8d9
Dan de Zille <dan@ddez.net>
2020-04-07 20:33:23 +0100

    Add pyenv and fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,3 +2,4 @@ set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/lib/ruby/gems/2.6.0/bin" $fish_user_paths
 set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 status --is-interactive; and source (rbenv init -|psub)
+status --is-interactive; and source (pyenv init -|psub)

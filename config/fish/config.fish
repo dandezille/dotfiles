631d59ea99891ca201f46d3dd38409669e20a762
Dan de Zille <dan@ddez.net>
2020-05-16 19:42:13 +0100

    Add cargo dir to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,5 @@
 set -gx fish_user_paths ".git/safe/../../bin" $fish_user_paths
+set -gx fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 status --is-interactive; and source (rbenv init -|psub)
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (nodenv init -|psub)

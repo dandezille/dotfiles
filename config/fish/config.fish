407152f0d2386f72da73f151e2b76c4fbc7b48fb
Dan de Zille <dan@ddez.net>
2020-05-19 18:18:51 +0100

    Set default fzf command to be rg

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,6 @@
 set -gx fish_user_paths ".git/safe/../../bin" $fish_user_paths
 set -gx fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
+set -gx FZF_DEFAULT_COMMAND 'rg --files'
 status --is-interactive; and source (rbenv init -|psub)
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (nodenv init -|psub)

2b339d4fb4489acfe7cf7d975ebfe0e0239332e3
Dan de Zille <dan@ddez.net>
2020-06-27 20:07:52 +0100

    Add clearance to rails template

--- rails_template.rb
+++ rails_template.rb
@@ -1,3 +1,5 @@
+gem 'clearance'
+
 gem_group :development, :test do
   gem 'awesome_print'
   gem 'rspec-rails'
@@ -16,11 +18,41 @@ create_file 'spec/support/factory_bot.rb',
 
 create_file 'spec/factories.rb', ''
 
-create_file 'spec/factories_spec.rb',
-            <<-EOL
+create_file 'spec/factories_spec.rb', <<-EOL
 require 'rails_helper'
 
 RSpec.describe FactoryBot do
   it { FactoryBot.lint traits: true }
 end
 EOL
+
+rails_command 'db:create'
+generate 'clearance:install'
+rails_command 'db:migrate'
+
+create_file 'app/views/layouts/_header.html.erb', <<-EOL
+<div id="header"> 
+  <% if signed_in? %>
+    Signed in as: <%= current_user.email %>
+    <%= button_to 'Sign out', sign_out_path, method: :delete %>
+  <% else %>
+    <%= link_to 'Sign in', sign_in_path %>
+  <% end %> 
+</div>
+EOL
+
+create_file 'app/views/layouts/_flash.html.erb', <<-EOL
+<div id="flash"> 
+  <% flash.each do |key, value| %>
+    <div class="flash <%= key %>"><%= value %></div>
+  <% end %>
+</div>
+EOL
+
+insert_into_file 'app/views/layouts/application.html.erb',
+                 "    <%= render partial: 'layouts/flash' %>\n",
+                 after: "<body>\n"
+
+insert_into_file 'app/views/layouts/application.html.erb',
+                 "    <%= render partial: 'layouts/header' %>\n",
+                 after: "<body>\n"

2b74c72230a71c86bef4a7d91fc3edc86f374a6a
Dan de Zille <dan@ddez.net>
2020-08-17 10:38:12 +0100

    Reorder PATH so local bin takes precident

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,7 @@
-set -gx fish_user_paths ".git/safe/../../bin" $fish_user_paths
-set -gx fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
-set -gx FZF_DEFAULT_COMMAND 'rg --files'
 status --is-interactive; and source (rbenv init -|psub)
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (nodenv init -|psub)
+set -gx FZF_DEFAULT_COMMAND 'rg --files'
+set -gx fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
+set -gx fish_user_paths ".git/safe/../../bin" $fish_user_paths
 starship init fish | source

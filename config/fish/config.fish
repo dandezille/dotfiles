14ac539ac4e0955c1e1dadeb83696a538d41df6b
Dan de Zille <dan@ddez.net>
2021-06-26 21:28:40 +0100

    Add cask default app dir

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,6 +6,7 @@ status --is-interactive; and source (rbenv init -|psub)
 status --is-interactive; and source (nodenv init -|psub)
 
 set -gx FZF_DEFAULT_COMMAND 'rg --files'
+set -gx HOMEBREW_CASK_OPTS '--appdir=~/Applications'
 
 set -gx fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -gx fish_user_paths "$HOME/.cargo/bin" $fish_user_paths

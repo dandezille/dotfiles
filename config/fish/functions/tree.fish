354bed85d42cc5e58bb0e4f799ee8706366da8df
Dan de Zille <dan@ddez.net>
2022-01-04 19:22:31 +0000

    Clean start with new macbook

--- config/fish/functions/tree.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function tree --wraps='exa -T' --description 'alias tree=exa -T'
-  exa -T $argv;
-end
--- scripts/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rcup -v -d $(PWD) -x README.md -x Brewfile
